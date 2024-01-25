4e715103c06f47bebaeedf49335e1a3b40b9e61f
Paul Gung <gongshaoxu@gmail.com>
2024-01-25 21:02:24 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
-import * as process from 'process'
-import * as path from 'path'
-import minimist from 'minimist' // 导入处理命令行参数方法
-import scanFolder from './libs/scanFolder' // 导入主方法
-import scanHelper from './libs/scanHelper' // 导入帮助方法
+import * as process from "process";
+import * as path from "path";
+import minimist from "minimist"; // 导入处理命令行参数方法
+import scanFolder from "./libs/scanFolder"; // 导入主方法
+import scanHelper from "./libs/scanHelper"; // 导入帮助方法
 
 function main(): void {
   // 获取命令行参数
-  const argv = minimist(process.argv.slice(2))
+  const argv = minimist(process.argv.slice(2));
   // 获取目录路径
-  const cwd:string = process.cwd()
+  const cwd: string = process.cwd();
 
   // 用户输入-h或-help时
   if (argv.h || argv.help) {
-    scanHelper()
+    scanHelper();
   } else {
     // 获取相对路径
-    const relativePath = argv.p || argv.path || 'dist'
+    const relativePath = argv.p || argv.path || "dist";
     // 拼接绝对路径
-    const folderPath:string = path.join(cwd, relativePath)
+    const folderPath: string = path.join(cwd, relativePath);
     // 运行主方法
-    scanFolder(folderPath)
+    const { result, errArray } = scanFolder(folderPath);
+    console.log("ES6 check result:", result);
+    console.log("ES6 check detail:", errArray);
   }
 }
 
 try {
-  main()
+  main();
 } catch (error) {
-  console.error('An error occurred in es6-check plugin:', error)
+  console.error("An error occurred in es6-check plugin:", error);
 }
-
-
-
--- src/libs/scanFolder/index.ts
+++ src/libs/scanFolder/index.ts
@@ -1,45 +1,50 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import scanCode from '../scanCode'
+import * as fs from 'fs';
+import * as path from 'path';
+import { globSync } from 'glob';
+import * as acorn from 'acorn';
 
-// 扫描文件夹中的所有 JavaScript 文件并检查是否包含 ES6 语法
-function scanFolder(folderPath):void {
-
-  // 鲁棒性检查，处理dist文件夹不存在的情况
-  let files = null
-  try {
-    // 如果没有读到dist文件夹则提示用户并返回
-    files = fs.readdirSync(folderPath, 'utf-8')
-  }catch (e){
-    console.log("没有读到dist文件夹，如果想指定目录，请使用--path，详细请运行命令:")
-    console.log("es6-check --help")
-    return
-  }
-
-  // 遍历读到的文件/文件夹
-  for (const file of files) {
-    const filePath:string = path.join(folderPath, file)
-    const stats:fs.Stats = fs.statSync(filePath)
-
-    if (stats.isDirectory()) {
-      // 递归扫描子文件夹
-      scanFolder(filePath)
-    } else if (stats.isFile() && file.endsWith('.js')) {
-      try {
-        // 扫描单文件代码
-        const code:string = fs.readFileSync(filePath, 'utf-8')
-        // ES6特性检查
-        const hasES6:boolean = scanCode(code)
-        if (hasES6) {
-          console.log(`文件 ${filePath} 中包含 ES6 语法`)
-        } else {
-          console.log(`文件 ${filePath} 不包含 ES6 语法`)
-        }
-      } catch (error) {
-        console.error(`读取文件 ${filePath} 失败：${error}`)
-      }
-    }
-  }
+interface IEs6CheckRes {
+  result: boolean;
+  errArray: Array<{ errorInfo; fileName }>;
 }
 
-export default scanFolder
+// 遍历读到的js文件
+const traverseFiles = (jsfiles: string[]) => {
+  // 错误栈
+  const errArray = [];
+  // 扫描结果
+  let result = false;
+
+  // jsfiles是文件名的字符串数组
+  for (const fileName of jsfiles) {
+    // 获取代码字符串
+    const code: string = fs.readFileSync(fileName, 'utf-8');
+
+    try {
+      // 如果不符合ES5则会转化报错
+      acorn.parse(code, { sourceType: 'script', ecmaVersion: 5 });
+    } catch (error) {
+      // 提取第一个换行符之前的错误信息
+      const errorInfo = error.stack.split('\n')[0];
+      const errorObj = {
+        errorInfo,
+        fileName,
+      };
+      errArray.push(errorObj);
+      // ES6特性检查，只要有一个文件包含es6语法，则返回true
+      result = true;
+    }
+  }
+
+  return { result, errArray };
+};
+
+// 扫描文件夹中的所有 JavaScript 文件并检查是否包含 ES6 语法
+const scanFolder = (folderPath: string): IEs6CheckRes => {
+  // 使用glob获取所有.js结尾的文件
+  const jsfiles: string[] = globSync(path.join(folderPath, '**/*.js'));
+  return traverseFiles(jsfiles);
+};
+
+export default scanFolder;
+
